12322e4c90590630dce454af1bd8b379bd6c3834
Zahari <zahari.cheyreckov999@gmail.com>
2024-01-16 10:52:23 +0200

    Introduce components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { RQPlanets } from './components/RQPlanets';
+import { Planets } from './components/Planets';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/planets'>Planets</Link>
+          </li>
+          <li>
+            <Link to='/rq-planets'>RQ Planets</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/planets' element={<Planets />} />
+        <Route path='/rq-planets' element={<RQPlanets />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/RQPlanets.js
@@ -0,0 +1,5 @@
+export const RQPlanets = () => {
+    return (
+        <div>RQPlanets</div>
+    );
+}
