22227b73673b69a65f961ff1bc6e7b8ae0d2d216
Zahari <zahari.cheyreckov999@gmail.com>
2024-01-16 11:09:41 +0200

    Fetch planets data with react query

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import { Link, Route, Routes } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 import { Home } from './components/Home';
 import { RQPlanets } from './components/RQPlanets';
 import { Planets } from './components/Planets';
 
 import './App.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/planets'>Planets</Link>
-          </li>
-          <li>
-            <Link to='/rq-planets'>RQ Planets</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/planets' element={<Planets />} />
-        <Route path='/rq-planets' element={<RQPlanets />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/planets'>Planets</Link>
+            </li>
+            <li>
+              <Link to='/rq-planets'>RQ Planets</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/planets' element={<Planets />} />
+          <Route path='/rq-planets' element={<RQPlanets />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQPlanets.js
+++ src/components/RQPlanets.js
@@ -1,5 +1,23 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:4000/planets');
+}
+
 export const RQPlanets = () => {
+    const { isLoading, data: planets } = useQuery('planets', fetchSuperHeroes);
+
+    if (isLoading) {
+        return <h1>Data is Loading...</h1>;
+    }
+
     return (
-        <h1>RQPlanets</h1>
+        <>
+            <h1>React Query Planets</h1>
+            {planets && planets?.data.map((planet) => {
+                return <div key={planet.id}>{planet.planet}</div>
+            })}
+        </>
     );
 }
